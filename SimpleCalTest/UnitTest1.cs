bc3cac2a0484516e87c5592a5b5b39e47be5ba0f
dnsrao <adarshnadig.s@live.com>
2023-11-16 22:26:23 +0530

    FirstCommit

---
+++ SimpleCalTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using SimpleCalculator;
+
+namespace SimpleCalTest;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(0, 0, 0)]
+    [InlineData(1, 2, 3)]
+    [InlineData(-2, 2, 0)]
+    [InlineData(-4, -6, -10)]
+    public void ShouldAddTwoValues_InlineData(int value1, int value2, int expectedResult)
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        var result = calculator.Add(value1, value2);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace SimpleCalculator;
+
+public class Calculator
+{
+
+    public double Add(double firstNumber,double secondNumber)
+    {
+        try
+        {
+            return firstNumber + secondNumber;  
+        }
+        catch(Exception ex)
+        {
+            return -1;
+        }
+
+        return -1;
+    }
+}
+
